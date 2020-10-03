60519f92cee6397c65f31285cab8fca6bb67e3fe
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2020-10-03 20:43:05 +0200

    add useState()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleAdd = () => setCounter( counter + 1); 
+
+  // const handleAdd = () => setCounter(prevValue => prevValue + 1); 
+
   return (
     <>
-      <h1>Hello</h1>
+      <h1>Licznik </h1>
+      <button onClick={handleAdd}>Add</button>
+      <p>{counter}</p>
     </>
   );
 }
